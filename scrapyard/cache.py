0a7ae979a15d12a75153aef5b2523b983ef9b6ac
Thomas Texier <sharkone@en-mousse.org>
2015-02-01 16:58:55 +0000

    Improve caching

--- scrapyard/cache.py
+++ scrapyard/cache.py
@@ -4,7 +4,9 @@ import hashlib
 import os
 import pickle
 import redis
+import sys
 import time
+import timeit
 import urlparse
 
 HOUR = datetime.timedelta(hours=1)
@@ -14,28 +16,45 @@ WEEK = datetime.timedelta(days=1)
 redis_url   = urlparse.urlparse(os.getenv('REDISCLOUD_URL', 'redis://{0}:{1}'.format(os.getenv('IP', '0.0.0.0'), 6379)))
 redis_cache = redis.StrictRedis(host=redis_url.hostname, port=redis_url.port, password=redis_url.password)
 
-def optional(func, expiration):
+def __get_cache(key):
+    start_time = timeit.default_timer()
+    try:
+        result = redis_cache.get(key)
+        result = pickle.loads(result) if result else result
+        sys.stdout.write('{0} : {1:3.1f}s : GET : {2}\n'.format('RDS:OK', timeit.default_timer() - start_time, key))
+        return result
+    except redis.exceptions.RedisError as exception:
+        sys.stderr.write('{0} : {1:3.1f}s : GET : {2} : {3}\n'.format('RDS:KO', timeit.default_timer() - start_time, key, repr(exception).replace(',)', ')')))
+
+def __set_cache(key, expiration, value):
+    start_time = timeit.default_timer()
+    try:
+        redis_cache.setex(key, int(expiration.total_seconds()), pickle.dumps(value))
+        sys.stdout.write('{0} : {1:3.1f}s : SET : {2}\n'.format('RDS:OK', timeit.default_timer() - start_time, key))
+    except redis.exceptions.RedisError as exception:
+        sys.stderr.write('{0} : {1:3.1f}s : SET : {2} : {3}\n'.format('RDS:KO', timeit.default_timer() - start_time, key, repr(exception).replace(',)', ')')))
+
+def optional(func, expiration, cache_key=None):
     def _optional(func, *args, **kwargs):
-        key   = hashlib.md5(':'.join([func.__name__, str(args), str(kwargs)])).hexdigest()
-        value = redis_cache.get(key)
+        key   = hashlib.md5(':'.join([func.__name__, str(args), str(kwargs)])).hexdigest() if not cache_key else cache_key
+        value = __get_cache(key)
 
         # Update if needed
-        value = pickle.loads(value) if value else value
         if not value or value['expires_on'] < datetime.datetime.now():
             data = func(*args, **kwargs)
             if data:
                 value = { 'expires_on': datetime.datetime.now() + expiration, 'data': data }
-                redis_cache.setex(key, int(expiration.total_seconds()), pickle.dumps(value))
+                __set_cache(key, expiration, value)
 
         # Return what we got
         return value['data'] if value else None
 
     return decorator.decorator(_optional, func)
 
-def mandatory(func, expiration, retry_delay, max_retries):
+def mandatory(func, expiration, retry_delay, max_retries, cache_key=None):
     def _mandatory(func, *args, **kwargs):
-        key   = hashlib.md5(':'.join([func.__name__, str(args), str(kwargs)])).hexdigest()
-        value = redis_cache.get(key)
+        key   = hashlib.md5(':'.join([func.__name__, str(args), str(kwargs)])).hexdigest() if not cache_key else cache_key
+        value = __get_cache(key)
 
         # Force initial update
         if not value:
@@ -44,18 +63,17 @@ def mandatory(func, expiration, retry_delay, max_retries):
                 data = func(*args, **kwargs)
                 if data:
                     value = { 'expires_on' : datetime.datetime.now() + expiration, 'data': data }
-                    redis_cache.setex(key, int(expiration.total_seconds()), pickle.dumps(value))
+                    __set_cache(key, expiration, value)
                     break
                 time.sleep(retry_delay)
                 retry_count -= 1
         else:
             # Update if needed
-            value = pickle.loads(value)
             if value['expires_on'] < datetime.datetime.now():
                 data = func(*args, **kwargs)
                 if data:
                     value = { 'expires_on': datetime.datetime.now() + expiration, 'data': data }
-                    redis_cache.setex(key, int(expiration.total_seconds()), pickle.dumps(value))
+                    __set_cache(key, expiration, value)
 
         # Return what we got
         return value['data'] if value else None
--- scrapyard/network.py
+++ scrapyard/network.py
@@ -11,31 +11,38 @@ TIMEOUT = 20
 ################################################################################
 # HTTP
 ################################################################################
-def http_get(url, params={}, headers={}, timeout=TIMEOUT, logging=True):
+def __http_get(request, timeout=TIMEOUT, logging=True):
     start_time = timeit.default_timer()
-
-    request = requests.Request('GET', url, params=params, headers=headers)
-    request = request.prepare()
 
     try:
         session  = requests.Session()
         response = session.send(request, timeout=timeout)
         response.raise_for_status()
         if logging:
-            sys.stdout.write('-> {0} : {1:4.2f}s : {2}\n'.format('NET', timeit.default_timer() - start_time, request.url))
+            sys.stdout.write('{0} : {1:3.1f}s : GET : {2}\n'.format('NET:OK', timeit.default_timer() - start_time, request.url))
         return response.content
     except requests.exceptions.RequestException as exception:
         if logging:
-            sys.stderr.write('-> {0} : {1:4.2f}s : {2} : {3}\n'.format('ERR', timeit.default_timer() - start_time, request.url, repr(exception).replace(',)', ')')))
+            sys.stderr.write('{0} : {1:3.1f}s : GET : {2} : {3}\n'.format('NET:KO', timeit.default_timer() - start_time, request.url, repr(exception).replace(',)', ')')))
         raise exception
 
 ################################################################################
+def http_get(url, params={}, headers={}, timeout=TIMEOUT, logging=True):
+    request = requests.Request('GET', url, params=params, headers=headers)
+    request = request.prepare()
+    return __http_get(request, timeout, logging)
+
+################################################################################
 def http_get_cached_mandatory(url, expiration, retry_delay=1, max_retries=20, params={}, headers={}, timeout=TIMEOUT):
-    return cache.mandatory(http_get, expiration=expiration, retry_delay=retry_delay, max_retries=max_retries)(url, params=params, headers=headers, timeout=timeout)
+    request = requests.Request('GET', url, params=params, headers=headers)
+    request = request.prepare()
+    return cache.mandatory(__http_get, expiration=expiration, retry_delay=retry_delay, max_retries=max_retries, cache_key=request.url)(request=request, timeout=timeout)
 
 ################################################################################
 def http_get_cached_optional(url, expiration, params={}, headers={}, timeout=TIMEOUT):
-    return cache.optional(http_get, expiration=expiration)(url, params=params, headers=headers, timeout=timeout)
+    request = requests.Request('GET', url, params=params, headers=headers)
+    request = request.prepare()
+    return cache.optional(http_get, expiration=expiration, cache_key=request.url)(url, params=params, headers=headers, timeout=timeout)
 
 ################################################################################
 # JSON
@@ -45,11 +52,11 @@ def json_get(url, params={}, headers={}, timeout=TIMEOUT):
 
 ################################################################################
 def json_get_cached_mandatory(url, expiration, retry_delay=1, max_retries=20, params={}, headers={}, timeout=TIMEOUT):
-    return cache.mandatory(json_get, expiration=expiration, retry_delay=retry_delay, max_retries=max_retries)(url, params=params, headers=headers, timeout=timeout)
+    return json.loads(http_get_cached_mandatory(url, expiration, retry_delay, max_retries, params, headers, timeout))
 
 ################################################################################
 def json_get_cached_optional(url, expiration, params={}, headers={}, timeout=TIMEOUT):
-    return cache.optional(json_get, expiration=expiration)(url, params=params, headers=headers, timeout=timeout)
+    return json.loads(http_get_cached_optional(url, expiration, params, headers, timeout))
 
 ################################################################################
 # RSS
@@ -59,8 +66,8 @@ def rss_get(url, params={}, headers={}, timeout=TIMEOUT):
 
 ################################################################################
 def rss_get_cached_mandatory(url, expiration, retry_delay=1, max_retries=20, params={}, headers={}, timeout=TIMEOUT):
-    return cache.mandatory(rss_get, expiration=expiration, retry_delay=retry_delay, max_retries=max_retries)(url, params=params, headers=headers, timeout=timeout)
+    return feedparser.parse(http_get_cached_mandatory(url, expiration, retry_delay, max_retries, params, headers, timeout))
 
 ################################################################################
 def rss_get_cached_optional(url, expiration, params={}, headers={}, timeout=TIMEOUT):
-    return cache.optional(rss_get, expiration=expiration)(url, params=params, headers=headers, timeout=timeout)
+    return feedparser.parse(http_get_cached_optional(url, expiration, params, headers, timeout))
