4f295bf7258634819dff694c5951388e01dcd814
Thomas Texier <sharkone@en-mousse.org>
2015-02-01 22:48:11 +0000

    Improve Redis connection handling

--- scrapyard/cache.py
+++ scrapyard/cache.py
@@ -13,12 +13,12 @@ HOUR = datetime.timedelta(hours=1)
 DAY  = datetime.timedelta(days=1)
 WEEK = datetime.timedelta(days=1)
 
-redis_url   = urlparse.urlparse(os.getenv('REDISCLOUD_URL', 'redis://{0}:{1}'.format(os.getenv('IP', '0.0.0.0'), 6379)))
-redis_cache = redis.StrictRedis(host=redis_url.hostname, port=redis_url.port, password=redis_url.password)
+redis_url = urlparse.urlparse(os.getenv('REDISCLOUD_URL', 'redis://{0}:{1}'.format(os.getenv('IP', '0.0.0.0'), 6379)))
 
 def __get_cache(key):
     start_time = timeit.default_timer()
     try:
+        redis_cache = redis.StrictRedis(host=redis_url.hostname, port=redis_url.port, password=redis_url.password)
         result = redis_cache.get(key)
         result = pickle.loads(result) if result else result
         #sys.stdout.write('{0} : {1:3.1f}s : GET : {2}\n'.format('RDS:OK', timeit.default_timer() - start_time, key))
@@ -29,6 +29,7 @@ def __get_cache(key):
 def __set_cache(key, expiration, value):
     start_time = timeit.default_timer()
     try:
+        redis_cache = redis.StrictRedis(host=redis_url.hostname, port=redis_url.port, password=redis_url.password)
         redis_cache.setex(key, int(expiration.total_seconds()), pickle.dumps(value))
         #sys.stdout.write('{0} : {1:3.1f}s : SET : {2}\n'.format('RDS:OK', timeit.default_timer() - start_time, key))
     except redis.exceptions.RedisError as exception:
