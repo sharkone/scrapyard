7a581ea0c29bfe95b4bc03bcac50ee7741a099ca
Thomas Texier <sharkone@en-mousse.org>
2015-02-02 18:04:00 +0000

    Renable multithreading

--- scrapyard/utils.py
+++ scrapyard/utils.py
@@ -1,9 +1,8 @@
 from multiprocessing.pool import ThreadPool
 
 def mt_map(func, iterable):
-    # thread_pool = ThreadPool()
-    # result      = thread_pool.map(func, iterable)
-    # thread_pool.close()
-    # thread_pool.join()
-    result = map(func, iterable)
+    thread_pool = ThreadPool()
+    result      = thread_pool.map(func, iterable)
+    thread_pool.close()
+    thread_pool.join()
     return result
