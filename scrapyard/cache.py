af86f49f2e8cc69dbe108d270765ff913d742272
Thomas Texier <sharkone@en-mousse.org>
2015-02-07 00:53:20 +0000

    Cache now using zlib compression

--- scrapyard/cache.py
+++ scrapyard/cache.py
@@ -5,6 +5,7 @@ import redis
 import sys
 import timeit
 import urlparse
+import zlib
 
 HOUR = datetime.timedelta(hours=1)
 DAY  = datetime.timedelta(days=1)
@@ -17,15 +18,16 @@ def get(key):
     start_time = timeit.default_timer()
     try:
         redis_cache = redis.StrictRedis(connection_pool=redis_pool)
-        result = redis_cache.get(key)
-        result = pickle.loads(result) if result else result
+        value = redis_cache.get(key)
+        value = zlib.decompress(value) if value else value
+        value = pickle.loads(value) if value else value
 
-        # if not result:
+        # if not value:
         #     sys.stdout.write('RDS:MIS : {0:3.1f}s : {1}\n'.format(timeit.default_timer() - start_time, key))
-        # elif 'expires_on' in result:
-        #     sys.stdout.write('RDS:{0} : {1:3.1f}s : {2}\n'.format('SUC' if result['expires_on'] > datetime.datetime.now() else 'EXP', timeit.default_timer() - start_time, key))
+        # elif 'expires_on' in value:
+        #     sys.stdout.write('RDS:{0} : {1:3.1f}s : {2}\n'.format('SUC' if value['expires_on'] > datetime.datetime.now() else 'EXP', timeit.default_timer() - start_time, key))
 
-        return result
+        return value
     except redis.exceptions.RedisError as exception:
         sys.stderr.write('RDS:ERR : {0:3.1f}s : {1} : {2}\n'.format(timeit.default_timer() - start_time, key, repr(exception).replace(',)', ')')))
         raise exception
@@ -34,8 +36,11 @@ def set(key, value, expiration=None):
     start_time = timeit.default_timer()
     try:
         redis_cache = redis.StrictRedis(connection_pool=redis_pool)
+        value = pickle.dumps(value)
+        value = zlib.compress(value, 9)
+
         if expiration:
-            redis_cache.setex(key, int(expiration.total_seconds()), pickle.dumps(value))
+            redis_cache.setex(key, int(expiration.total_seconds()), value)
         else:
             redis_cache.set(key, pickle.dumps(value))
         # sys.stdout.write('RDS:SUC : {0:3.1f}s : {1}\n'.format(timeit.default_timer() - start_time, key))
